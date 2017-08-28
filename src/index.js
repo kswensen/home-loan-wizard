04f85ca60c27f2a5f4a668f53ce6a66f16c8c983
StayinNasty <keganswensen@gmail.com>
2017-08-28 16:39:08 -0600

    Finished it all and got the redux store to work

--- src/index.js
+++ src/index.js
@@ -3,7 +3,16 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
+import { Provider } from 'react-redux';
+import { HashRouter } from 'react-router-dom';
+import store from './store';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <HashRouter>
+        <Provider store={store}>
+            <App />
+        </Provider>
+    </HashRouter>
+    , document.getElementById('root'));
 registerServiceWorker();
 
